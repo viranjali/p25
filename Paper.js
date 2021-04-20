e9aca8f91fa175e29688570b9de7aaa876a39fdd
viranjali <76208580+viranjali@users.noreply.github.com>
2021-04-20 18:05:22 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.3,
+			friction:0,
+			density:1.1
+		}
+		this.x=x;
+		this.y=y;
+		this.r=r
+        this.image = loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var pos=this.body.position;		
+			push()
+			translate(pos.x, pos.y);
+			imageMode(CENTER)
+			fill("red");
+			image(this.image,0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
